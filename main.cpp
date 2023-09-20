65ab7c21adda2f1acfb66d65a8631a443e827839
andrelarrosa <andre.larrosa@outlook.com>
2023-09-19 22:21:47 -0300

    Extraindo bits

--- main.cpp
+++ main.cpp
@@ -1,36 +1,55 @@
 #include "EasyBMP.h"
+
 using namespace std;
+
+template <typename T>
+inline T extract_bits (const T v, const unsigned bstart, const unsigned blength) {
+	const T mask = (1 << blength) - 1;
+
+	return ((v >> bstart) & mask);
+}
+
+template <typename T>
+inline T set_bits (const T v, const unsigned bstart, const unsigned blength, const T bits) {
+	const T mask = ((1 << blength) - 1) << bstart; 
+
+	return (v & ~mask) | (bits << bstart);
+}
+
 int hideMessage(BMP &image, const string &message)
 {
 	int width = image.TellWidth();
     int height = image.TellHeight();
-   
-    int messageIndex = 0;
-     
-    for (int y = 0; y < height; ++y) {
-       for (int x = 0; x < width; ++x) {
-           RGBApixel pixel = image.GetPixel(x, y);
-           if (messageIndex < message.length()) {
-               
-               cout << messageIndex;
-               ++messageIndex;
-           }
-       }
-   }
+	
+	
     
    return 0;
 }
 
 int main() {
  	BMP image;
+ 	//Abre imagem
     if (!image.ReadFromFile("./sample/EasyBMPtext.bmp")) {
         cerr << "Erro ao abrir a imagem de entrada." << endl;
         return 1;
+    } else {
+    	cout << "imagem de entrada aberta com sucesso" << endl;
     }
     
-    std::string message = "testeFoto";
+    BMP image_copy(image);
     
-    hideMessage(image, message);
+    unsigned x = 0;
+	x = set_bits(x, 1, 2, static_cast<unsigned>(0x01));
+	cout << x << endl;
+	unsigned y = 7; // 8 bits e multiplos de 7
+	y = extract_bits(y, static_cast<unsigned>(0x01),1);
+	cout << y << endl;
+    
+    cout << "copiando a imagem..." << endl;
+    //Copia Imagem
+    image_copy.WriteToFile("./sample/test_copy.bmp");
+    cout << "copiado com sucesso!" << endl;
 
+	return 0;
 }
 
